b07a6e2c665807e57b5cc62093d61caa245f3a6a
putin <putin3martin3@gmail.com>
2026-04-04 13:28:47 +0900

    fix middleware for sitemap and robots

--- middleware.ts
+++ middleware.ts
@@ -58,6 +58,6 @@ export async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|public|google).*)'],
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|public|google|sitemap|robots).*)'],
 };
 
